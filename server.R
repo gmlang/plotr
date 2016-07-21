f826f099fc133671f2f3d90842f72d97f4054384
sliu102 <sliu102@hawk.iit.edu>
2016-07-21 14:23:30 -0500

    ggplot2

--- server.R
+++ server.R
@@ -1,11 +1,32 @@
 library(shiny)
+library(ggplot2)
 
 shinyServer(
-  
-  function(input, output) {
-    
-    output$hist <- renderPlot({
-      hist(rnorm(input$num))
-    })
-  }
-)
+        function(input, output) {
+                
+                filedata <- reactive({
+                        infile <- input$datafile
+                        if (is.null(infile)) {
+                                # User has not uploaded a file yet
+                                return(NULL)
+                        }
+                        read.csv(infile$datapath)
+                })
+                
+                output$x <- renderUI({
+                        df <-filedata()
+                        if (is.null(df)) return(NULL)
+                        
+                        items=names(df)
+                        names(items)=items
+                        selectInput("var", "Variable:",items)
+                        
+                })
+                
+                
+                output$plot <- renderPlot({
+                        ggplot(filedata(), aes(x = EngDispl)) + 
+                        geom_histogram() 
+                })
+                
+})
