ad4e9b3dc78161ffea82cbbbdd81ca8c02960358
Guangming Lang <gmlang@cabaceo.com>
2016-08-21 14:46:45 -0400

    update server.R

--- server.R
+++ server.R
@@ -1,17 +1,25 @@
 # change max upload file size to 30 MB, default is 5 MB
-options(shiny.maxRequestSize=30*1024^2) 
+options(shiny.maxRequestSize=30*1024^2, shiny.reactlog = TRUE) 
 
 server = function(input, output) {
-    datafile = callModule(upload, "datafile")
+    datafile = callModule(upload, "datafile") # datafile is a reactive expression, use it as if it's a function, datafile()
     
     output$datastruct = renderPrint({ str(datafile()) })
     
     output$first100rows = renderDataTable({
-        dat = datafile()
-        show_nrows = 100
-        if (nrow(dat) > show_nrows) dat[1:show_nrows,] 
-        else dat
+            show_nrows = 100
+            dat = datafile() # calling datafile() returns a data frame
+            if (nrow(dat) > show_nrows) dat[1:show_nrows,] 
+            else dat
     })
+    
+    output$plot_type <- renderUI({
+            dat = datafile()
+            radioButtons("plot_type", "Plot Type",
+                         c("Histogram", "QQ plot", "Barplot"), 
+                         selected = NULL)
+    })
+    
 }
 
 
